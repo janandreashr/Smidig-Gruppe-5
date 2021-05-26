c84cb6c2fe5b921659c2b5ab84025cf6a54fca7e
Espen Bakken <54417701+espenbakken@users.noreply.github.com>
2021-05-26 14:52:18 +0200

    Moved pictures

--- Frontpage/data.js
+++ Frontpage/data.js
@@ -1,31 +1,31 @@
 const data = [
     {
         id: 1,
-        image: 'images/p_luktboks.jpg',
+        image: '../images/p_luktboks.jpg',
         text: 'Mennesket og andre dyr har i grunn seks spesialsanser: Syn, hørsel, smak, lukt, berøring samt likevektsansen. Disse sansene er vi nødt til å stimulere i blant og vi har med dette produktet klart å tilfredstille hvertfall tre av våre hovedsanser: <br><br>' + 
         'Synet, hørselen, og luktesansen<br><br>' +
         'Med god universell utforming kan du tilpasse produktet slik at det passer akkurat deg eller de du har rundt deg på best mulig måte. Tilpass det til ditt bruk og få stimulert dine sanser du også!'
     }, 
     {
         id: 2,
-        image: 'images/p_det_sanselige.jpg',
+        image: '../images/p_det_sanselige.jpg',
         text: 'Lukten av 1700 - tallet<br><br>'+
         'Utstillingen «Det sanselige 1700-tallet» er det nærmeste vi kan komme en tidsmaskin. Den gir publikum mulighet til å oppleve og reflektere over hvordan folk som levde gjennom 1700-tallet opplevde sine omgivelser – gjennom sansene: Hva de så rundt seg, hørte av lyder, smakte, luktet og kjente. For å sette sanseopplevelsene i ett større perspektiv er de satt i sammenheng med tidlig globalisering, sjøveis handel og de norske regionenes særtrekk.'
     },
     {
         id: 3,
-        image: 'images/p_einbitavhistoria.jpg',
+        image: '../images/p_einbitavhistoria.jpg',
         text: 'Lukten av 1700 - tallet<br><br>'+
         'Utstillingen «Det sanselige 1700-tallet» er det nærmeste vi kan komme en tidsmaskin. Den gir publikum mulighet til å oppleve og reflektere over hvordan folk som levde gjennom 1700-tallet opplevde sine omgivelser – gjennom sansene: Hva de så rundt seg, hørte av lyder, smakte, luktet og kjente. For å sette sanseopplevelsene i ett større perspektiv er de satt i sammenheng med tidlig globalisering, sjøveis handel og de norske regionenes særtrekk.'
     },
     {
         id: 4,
-        image: 'images/p_historiske_databaser.jpg',
+        image: '../images/p_historiske_databaser.jpg',
         text: 'Historiske databaser'
     },
     {
         id: 5,
-        image: 'images/p_luktboks.jpg',
+        image: '../images/p_luktboks.jpg',
         text: 'Luktboks'
     },
 ]
