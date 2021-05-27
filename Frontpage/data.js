ca14804ccb02c4052461305abe7c9544a14cea89
Espen Bakken <54417701+espenbakken@users.noreply.github.com>
2021-05-27 09:58:04 +0200

    Endring av innhold og lagt til hoverZoom

--- Frontpage/data.js
+++ Frontpage/data.js
@@ -2,30 +2,33 @@ const data = [
     {
         id: 1,
         image: '../images/p_luktboks.jpg',
+        headline: 'Luktboks',
         text: 'Mennesket og andre dyr har i grunn seks spesialsanser: Syn, hørsel, smak, lukt, berøring samt likevektsansen. Disse sansene er vi nødt til å stimulere i blant og vi har med dette produktet klart å tilfredstille hvertfall tre av våre hovedsanser: <br><br>' + 
         'Synet, hørselen, og luktesansen<br><br>' +
         'Med god universell utforming kan du tilpasse produktet slik at det passer akkurat deg eller de du har rundt deg på best mulig måte. Tilpass det til ditt bruk og få stimulert dine sanser du også!'
     }, 
     {
         id: 2,
-        image: '../images/p_det_sanselige.jpg',
-        text: 'Lukten av 1700 - tallet<br><br>'+
-        'Utstillingen «Det sanselige 1700-tallet» er det nærmeste vi kan komme en tidsmaskin. Den gir publikum mulighet til å oppleve og reflektere over hvordan folk som levde gjennom 1700-tallet opplevde sine omgivelser – gjennom sansene: Hva de så rundt seg, hørte av lyder, smakte, luktet og kjente. For å sette sanseopplevelsene i ett større perspektiv er de satt i sammenheng med tidlig globalisering, sjøveis handel og de norske regionenes særtrekk.'
+        image: '../images/p_oslohavn.jpg',
+        headline: 'Oslo Havn 1798',
+        text: 'Oslo havn 1798 har som mål å gi publikum, både barn og voksne, innblikk i Oslos og Norges historie på slutten av 1700-tallet på nye måter. Vi oppfordrer til at den oppleves stående ute i Bjørvikaområdet, men den kan også oppleves bare på skjerm. Oslo havn 1798 er utviklet av Tidvis AS. Prosjektet Oslo havn 1798 er en interaktiv opplevelse langs Oslo havn (Havnepromenaden) som gir publikum – både barn og voksne – ett sanselig inntrykk av Oslos og Norges historie og kunsthistorie på slutten av 1700-tallet. Visuelt og lydmessig er Oslo havn 1798 en digital verden. Den er likevel fortøyd i den fysiske verden gjennom luktbokser og vektleggingen av stedet.'
     },
     {
         id: 3,
         image: '../images/p_einbitavhistoria.jpg',
-        text: 'Lukten av 1700 - tallet<br><br>'+
-        'Utstillingen «Det sanselige 1700-tallet» er det nærmeste vi kan komme en tidsmaskin. Den gir publikum mulighet til å oppleve og reflektere over hvordan folk som levde gjennom 1700-tallet opplevde sine omgivelser – gjennom sansene: Hva de så rundt seg, hørte av lyder, smakte, luktet og kjente. For å sette sanseopplevelsene i ett større perspektiv er de satt i sammenheng med tidlig globalisering, sjøveis handel og de norske regionenes særtrekk.'
+        headline: 'Veøya: Ein bit av historia',
+        text: 'Korleis kan det ha vore i Møre og Romsdal i vikingtida og i mellomalderen? Korleis såg husa, reiskapen og landskapet ut? Ved å kombinere kulturhistorisk forsking med nye digitale verktøy vil prosjektet Ein bit av historia utforske dei nye moglegheitene som har opna seg for å formidle historie og kulturarv. Her kan du utforske korleis ein liten del av Veøya utanfor Molde kan ha sett ut omkring år 1000 e.Kr. Du kan oppleve det enten i nettlesaren til dømes med mobil, eller du kan laste det ned og utforske slik ein gjer i dataspel. Denne opplevinga er eit pilotprosjekt som i liten skala viser korleis digital formidling av Møre og Romsdals viking- og mellomalderhistorie kan gjerast.'
     },
     {
         id: 4,
         image: '../images/p_historiske_databaser.jpg',
-        text: 'Historiske databaser'
+        headline: 'Historiske databaser',
+        text: 'Historiske databaser inneholder et bredt spekter av transkriberte og tilgjengeliggjorte historiske kilder til forskning, skole og allmennheten. Prosjektet har transkribert og standardisert summariske vare- og skipsanløpslister fra samtlige norske tollsted gjennom et utvalg år på 1700-tallet, og gjort dette tilgjengelig i to søkbare databaser. Varelistene viser hvilke varer som ble ført sjøveis til eller fra de norske tollstedene, samt mengdene, destinasjon/opphavsland, og tidvis verdien på varene. Skipsanløpslistene gir detaljer om skipenes størrelse, reiserute og kapteiner. Det har også kommet til en database med priser fra norske steder gjennom et utvalg år på 1700-tallet, samt en database som detaljerer kobbereksporten fra Trondheim fra 1751 til 1793.'
     },
     {
         id: 5,
-        image: '../images/p_luktboks.jpg',
-        text: 'Luktboks'
+        image: '../images/p_det_sanselige.jpg',
+        headline: 'Lukten av 1700 - tallet',
+        text: 'Utstillingen «Det sanselige 1700-tallet» er det nærmeste vi kan komme en tidsmaskin. Den gir publikum mulighet til å oppleve og reflektere over hvordan folk som levde gjennom 1700-tallet opplevde sine omgivelser – gjennom sansene: Hva de så rundt seg, hørte av lyder, smakte, luktet og kjente. For å sette sanseopplevelsene i ett større perspektiv er de satt i sammenheng med tidlig globalisering, sjøveis handel og de norske regionenes særtrekk.'
     },
 ]
--- Frontpage/script.js
+++ Frontpage/script.js
@@ -21,24 +21,42 @@ function myFunction(){
         const newDiv = document.createElement("div");
         const textDiv = document.createElement("div");
         const newImg = document.createElement("img");
+        const headline = document.createElement("h2");
         const pTag = document.createElement("p");
     
         newDiv.setAttribute("data-aos-duration", "3000");
         newDiv.setAttribute("data-aos-anchor-placement", "center-bottom");
         newDiv.setAttribute("class", "aos-init");
-        newDiv.setAttribute("data-aos-offset", "500")
+        newDiv.setAttribute("data-aos-offset", "500");
         newImg.setAttribute("src", e.image);
         textDiv.setAttribute("class", "text");
        
         newDiv.appendChild(textDiv);
         
+        headline.innerHTML = e.headline;
+        
+        headline.setAttribute("class", "text-front");
         pTag.setAttribute("class", "text-front");
+        textDiv.appendChild(headline);
         pTag.innerHTML = e.text;
         textDiv.appendChild(pTag);
         const currentDiv =  document.getElementById("container");
         currentDiv.appendChild(newDiv);
-       
-    
+
+        newImg.addEventListener("mouseover", function(event){
+            event.target.style.transform ="scale(2)";
+            event.target.style.boxShadow ="rgba(0, 0, 0, 0.4) 0px 30px 90px";
+            pTag.style.opacity="0.4";
+            headline.style.opacity="0.4";
+        })
+
+        newImg.addEventListener("mouseleave", function(event){
+            event.target.style.transform ="scale(1)";
+            event.target.style.boxShadow ="rgba(0, 0, 0, 0.35) 0px 5px 15px";
+            document.body.style.opacity="1";
+            pTag.style.opacity="1";
+            headline.style.opacity="1";
+        })
     
         if (e.id%2 == 0){
             newDiv.setAttribute("id", "test2");
