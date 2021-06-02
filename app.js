eeb8a5a8dd15e46991c1dbd5065fa2b1b89e3a6d
Siddii190696 <56352781+Siddii190696@users.noreply.github.com>
2021-06-02 13:37:47 +0200

    Update app.js
    active effect form

--- app.js
+++ app.js
@@ -64,3 +64,23 @@ const formEvent = form.addEventListener('submit', event => {
     localStorage.clear();
     window.location.reload();
 });
+
+//contact 
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
