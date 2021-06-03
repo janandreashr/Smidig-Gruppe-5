9d9bb3b61085f756b7a7d2f6e9c8d301c602c204
Espen Bakken <54417701+espenbakken@users.noreply.github.com>
2021-06-03 09:39:21 +0200

    Added favicon

--- Frontpage/data.js
+++ Frontpage/data.js
@@ -1,32 +1,41 @@
 const data = [
     {
         id: 1,
-        image: '../images/p_luktboks.jpg',
-        headline: 'Luktboks',
-        text: 'Mennesket og andre dyr har i grunn seks spesialsanser: Syn, hørsel, smak, lukt, berøring samt likevektsansen. Disse sansene er vi nødt til å stimulere i blant og vi har med dette produktet klart å tilfredstille hvertfall tre av våre hovedsanser: <br><br>' + 
-        'Synet, hørselen, og luktesansen<br><br>' +
-        'Med god universell utforming kan du tilpasse produktet slik at det passer akkurat deg eller de du har rundt deg på best mulig måte. Tilpass det til ditt bruk og få stimulert dine sanser du også!'
+        image: '../images/p_tidvis.png',
+        headline: 'Velkommen til Tidvis!',
+        text: 'Tidvis er et ideelt selskap som utforsker og utvikler nye måter å formidle og tilgjengeliggjøre forskning på, med et spesielt fokus på historie. Vi mener historie er viktig, fordi det danner utgangspunktet for vår forståelse av nåtiden og hva vi forventer av fremtiden. <br> <br>' + 
+        'Vår målsetning er å benytte både velkjente og nye virkemidler og teknologier. Eksempler på slike er databaser, spill, 3D-modeller, augmented og virtual reality, animasjoner, interaktive sanseopplevelser og god, fleksibel design på alle flater. <br><br>' + 
+        'Vi ønsker å nå bredt ut til flest mulig med elegante løsninger. Slik gjør vi det lettere for mennesker i dag å få erfaring med fortiden, og legger til rette for at flere får et bredere kunnskapsgrunnlag for å reflektere over nåtiden og fremtiden.'
     }, 
     {
         id: 2,
+        image: '../images/p_luktboks.jpg',
+        headline: 'Luktboks',
+        text: 'I mangel på tidsmaskin er sanseopplevelser kanskje det som lettest kan hjelpe moderne mennesker å forstå hvordan folk i tidligere tider opplevde verden. Luktboksen gir mulighet til nettopp å utforske dette.<br><br>' + 
+        'Opplevelsen av fortidige lukter vil aldri bli helt den samme som den fortidige mennesker hadde – til det er omgivelsene for forskjellige. Lukt kan likevel være et utgangspunkt som kan sette i gang refleksjoner om hvordan det kan ha vært å leve for 100, 300 eller kanskje 1000 år siden. <br><br>' + 
+        'Stimulering av sansene som formidlingsmetode i historie, kunst og kultur er foreløpig i startgropen internasjonalt og til nå lite brukt i Norge. I dette tilfellet har 3D-printing i kombinasjon med forskningskompetanse i historie, vært sentralt for å muliggjøre både utvikling og produksjon av Luktboksen.'
+
+    },
+    {
+        id: 3,
         image: '../images/p_oslohavn.jpg',
         headline: 'Oslo Havn 1798',
         text: 'Oslo havn 1798 har som mål å gi publikum, både barn og voksne, innblikk i Oslos og Norges historie på slutten av 1700-tallet på nye måter. Vi oppfordrer til at den oppleves stående ute i Bjørvikaområdet, men den kan også oppleves bare på skjerm. Oslo havn 1798 er utviklet av Tidvis AS. Prosjektet Oslo havn 1798 er en interaktiv opplevelse langs Oslo havn (Havnepromenaden) som gir publikum – både barn og voksne – ett sanselig inntrykk av Oslos og Norges historie og kunsthistorie på slutten av 1700-tallet. Visuelt og lydmessig er Oslo havn 1798 en digital verden. Den er likevel fortøyd i den fysiske verden gjennom luktbokser og vektleggingen av stedet.'
     },
     {
-        id: 3,
+        id: 4,
         image: '../images/p_einbitavhistoria.jpg',
         headline: 'Veøya: Ein bit av historia',
         text: 'Korleis kan det ha vore i Møre og Romsdal i vikingtida og i mellomalderen? Korleis såg husa, reiskapen og landskapet ut? Ved å kombinere kulturhistorisk forsking med nye digitale verktøy vil prosjektet Ein bit av historia utforske dei nye moglegheitene som har opna seg for å formidle historie og kulturarv. Her kan du utforske korleis ein liten del av Veøya utanfor Molde kan ha sett ut omkring år 1000 e.Kr. Du kan oppleve det enten i nettlesaren til dømes med mobil, eller du kan laste det ned og utforske slik ein gjer i dataspel. Denne opplevinga er eit pilotprosjekt som i liten skala viser korleis digital formidling av Møre og Romsdals viking- og mellomalderhistorie kan gjerast.'
     },
     {
-        id: 4,
+        id: 5,
         image: '../images/p_historiske_databaser.jpg',
         headline: 'Historiske databaser',
         text: 'Historiske databaser inneholder et bredt spekter av transkriberte og tilgjengeliggjorte historiske kilder til forskning, skole og allmennheten. Prosjektet har transkribert og standardisert summariske vare- og skipsanløpslister fra samtlige norske tollsted gjennom et utvalg år på 1700-tallet, og gjort dette tilgjengelig i to søkbare databaser. Varelistene viser hvilke varer som ble ført sjøveis til eller fra de norske tollstedene, samt mengdene, destinasjon/opphavsland, og tidvis verdien på varene. Skipsanløpslistene gir detaljer om skipenes størrelse, reiserute og kapteiner. Det har også kommet til en database med priser fra norske steder gjennom et utvalg år på 1700-tallet, samt en database som detaljerer kobbereksporten fra Trondheim fra 1751 til 1793.'
     },
     {
-        id: 5,
+        id: 6,
         image: '../images/p_det_sanselige.jpg',
         headline: 'Lukten av 1700 - tallet',
         text: 'Utstillingen «Det sanselige 1700-tallet» er det nærmeste vi kan komme en tidsmaskin. Den gir publikum mulighet til å oppleve og reflektere over hvordan folk som levde gjennom 1700-tallet opplevde sine omgivelser – gjennom sansene: Hva de så rundt seg, hørte av lyder, smakte, luktet og kjente. For å sette sanseopplevelsene i ett større perspektiv er de satt i sammenheng med tidlig globalisering, sjøveis handel og de norske regionenes særtrekk.'
